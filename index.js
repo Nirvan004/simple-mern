b021eaf864d3e20d3564696862225f575c5b5077
Nirvan004 <Nirvanandb2004@gmail.com>
2026-01-29 14:27:47 -0500

    bug fixing

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const app = express();
 
 const routeTasks = require('./src/routes/tasks');
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+//app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.json());
 
 app.use('/api/tasks', routeTasks, (req, res) => res.sendStatus(401));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+//app.get('*', (req, res) => {
+//  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+//});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
