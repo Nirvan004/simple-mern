88d90ef57ae693dc327815f3698ee8192d209874
Nirvan004 <Nirvanandb2004@gmail.com>
2026-01-29 14:42:54 -0500

    bugs suck

--- src/db.js
+++ src/db.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI);
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error("MongoDB connection error:", err));
 
 module.exports = mongoose;
